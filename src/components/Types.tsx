dc869ab3e842e6756c535305da6da03025a221ed
Nikhil <nikhilsimontoppo@gmail.com>
2022-10-21 12:35:33 +0530

    wierd

--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -4,30 +4,37 @@ function Types({ final }) {
   const [types, setTypes] = useState([]);
 
   useEffect(() => {
-  async  function getTypes() {
+    async function getTypes() {
       const newArr = [];
       final.map(async (item) => {
         const respType = await fetch(item.move.url);
         const respTypeData = await respType.json();
         const final2 = respTypeData.type.name;
-
+        console.log(final2);
         newArr.push(final2);
         setTypes(newArr);
       });
+      console.log(types);
     }
     getTypes();
-  });
+  }, [final]);
 
   return (
-    <ul className="text-3xl flex gap-6 flex-col items-center">
-   {
-    types.map((type,key)=>{
-      return (
-        <li key={key} className="w-[100px]" > <img className="w-[100px]" src={`src/assets/${type}.svg`} alt="" /></li>
-        )
-    })
-   }
-    </ul>
+    <div>
+      <ul className="text-3xl flex gap-6 flex-col items-center">
+        {types.map((type, key) => {
+          return (
+            <li key={key} className="w-[100px]">
+              <img
+                className="w-[100px]"
+                src={`src/assets/${type}.svg`}
+                alt=""
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
