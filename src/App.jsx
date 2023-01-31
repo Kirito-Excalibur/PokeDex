7bcf26efc570f5bef0f51740b5d8a9c199bff478
Nikhil <nikhilsimontoppo@gmail.com>
2023-01-31 22:34:47 +0530

    Adusted responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [maxVal, setmaxVal] = useState(40);
   const [searchVal, setsearchVal] = useState("");
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     async function getArray() {
       console.log(searchVal);
       const resp = await fetch(
@@ -16,7 +16,6 @@ function App() {
       );
       const respData = await resp.json();
       const arr = respData.results;
-    
 
       const imges = arr.map(async (imgd) => {
         const resp1 = await fetch(imgd.url);
@@ -25,7 +24,7 @@ function App() {
         return resp1Data;
       });
       setImgdata(await Promise.all(imges));
-      setLoading(false)
+      setLoading(false);
     }
 
     getArray();
@@ -39,54 +38,42 @@ function App() {
   }
 
   return (
-    <div className="mx-auto flex flex-col items-center relative z-10 justify-center p-5 bg-red-600 w-[420px] sm:w-[1810px]  ">
+    <div className="mx-auto flex flex-col items-center relative z-10 justify-center p-5 bg-red-600   ">
       <h1 className="text-center text-3xl pb-4">PokéDex</h1>
       <input
         type="text"
         placeholder="Search..."
         className="pl-2"
-        onChange={(e) =>{ 
-          setsearchVal(e.target.value)
-          setmaxVal(647)
+        onChange={(e) => {
+          setsearchVal(e.target.value);
+          setmaxVal(647);
         }}
       />
       <div className="flex flex-wrap mt-5 gap-4  justify-center items-center">
+        {imgdata
+          .filter((val) => {
+            if (searchVal == "") {
+              return val;
+            } else if (String(val.name).includes(searchVal)) {
+              return val;
+            }
+          })
+          .map((name, key) => {
+            return (
+              <div className="flex flex-col justify-center items-center">
+                <Link key={key} to={`pokemon/${name.id}`}>
+                  <h1>{name.id}</h1>
 
-
-        
-        {imgdata.filter((val)=>{
-          if(searchVal==""){
-            return val
-          }
-          else if(String(val.name).includes(searchVal)){
-            return val
-          }
-        }).map((name, key) => {
-          return (
-            <div className="flex flex-col justify-center items-center">
-
-
-              <Link key={key} to={`pokemon/${name.id}`}>
-                <h1>{name.id}</h1>
-
-         
-                <img
-                  loading="eager"
-                  className="w-[150px] h-[150px] rounded-xl bg-white border border-green-500"
-                  src={name.sprites.front_default}
-                />
- 
-
-              </Link>
-              <h1 className="text-2xl">{capitalizeFirstLetter(name.name)}</h1>
-
-
-
-            </div>
-          );
-        })}
-
-        
+                  <img
+                    loading="eager"
+                    className="w-[150px] h-[150px] rounded-xl bg-white border border-green-500"
+                    src={name.sprites.front_default}
+                  />
+                </Link>
+                <h1 className="text-2xl">{capitalizeFirstLetter(name.name)}</h1>
+              </div>
+            );
+          })}
       </div>
 
       <button className="mt-5" onClick={() => setmaxVal(maxVal + 20)}>
